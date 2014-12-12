2891cf28ffca9f43e7ac0d8e72bcb0796f0ba830
Arnas Dundulis <arrnas@gmail.com>
2014-12-12 21:25:44 +0200

    added swiftyJson, singleton for communication with the backend service

--- chatApp/Backend.swift
+++ chatApp/Backend.swift
@@ -9,11 +9,30 @@
 import Foundation
 
 class Backend {
-    var username : String = ""
-    var password : String = ""
-    var access_token : String = ""
-    let manager = AFHTTPRequestOperationManager()
+    var access_token = ""
+    let manager = AFHTTPRequestOperationManager(baseURL: NSURL(string:"https://chat-app-backend.herokuapp.com/v1/"))
+    class var sharedInstance: Backend {
+        struct Static {
+            static let instance : Backend = Backend()
+        }
+        return Static.instance
+    }
     
     init() {
+        let params = ["username":"test","password":"pass"]
+        manager.POST("login",
+                    parameters: params,
+                    success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                        let json = JSON(responseObject)
+                        if let token = json["access_token"].string {
+                            println("access token = \(token)")
+                        } else {
+                            println(json.error)
+                        }
+                        
+                    },
+                    failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                        println("Error: " + error.localizedDescription)
+                    })
     }
 }
